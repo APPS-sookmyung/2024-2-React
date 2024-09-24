fe893b182e1bdf1637f73555ae5b2791def3f998
suminb99 <suminbaek99@gmail.com>
2024-09-24 13:53:46 +0900

    Feat: 2주차 내용 정리 및 실습 코드

--- 백수민/first-project/src/components/Product.jsx
+++ 백수민/first-project/src/components/Product.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
-export default function Product() {
+// export default function Product(props) {
+
+//   console.log(props);
+//   return (
+//     <div>
+//       <img src={props.url} alt={props.alt}/>
+//       <div>{props.name}</div>
+//       <div>{props.price}</div>
+//     </div>
+//   )
+// }
+
+// 객체 구조 분해 할당(비구조화 할당)
+export default function Product({url, alt, name, price}) {
   return (
-    <div>Product</div>
+    <div>
+      <img src={url} alt={alt}/>
+      <div>{name}</div>
+      <div>{price}</div>
+    </div>
   )
 }
+
