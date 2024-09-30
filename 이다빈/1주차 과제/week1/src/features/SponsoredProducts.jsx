e83e3dbe1ae3da5fbda6fdefbf2d1ad68095609c
ninum12 <ninum@sookmyung.ac.kr>
2024-10-01 01:07:11 +0900

    Feat: 3주차 실습 코드

---
+++ 이다빈/1주차 과제/week1/src/features/SponsoredProducts.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function SponsoredProducts() {
+  return (
+    <div>SponsoredProducts</div>
+  )
+}
+
--- 이다빈/2주차 실습/week2/src/components/Product.jsx
+++ 이다빈/2주차 실습/week2/src/components/Product.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export default function Product({url, alt, name, price}) {
-// props 대신 {url, alt, name, price} 이용
+export default function Product({z}) {
+  // 객체 구조 분해 할당(비구조화 할당)
+  // props 대신 {url, alt, name, price} 사용
+  // props로 어떤 데이터를 받고 있는가를 한눈에 확인 가능
+
   return (
     <div>
       <img src={url} alt={alt}/> {/* 이미지 url, 대체 텍스트 */}
