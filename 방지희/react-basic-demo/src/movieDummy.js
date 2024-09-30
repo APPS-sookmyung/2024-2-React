975f9aa21abcb7816b0289b6b9d9dbfbf85ab53c
Bangjh05 <everangel05@naver.com>
2024-10-01 01:21:44 +0900

    Feat: 3주차 실습 코드

---
+++ 방지희/react-basic-demo/src/movieDummy.js
@@ -0,0 +1,64 @@
+export const dummy = {
+  results: [
+    {
+      title: "베테랑2",
+      poster_path:
+        "https://search.pstatic.net/common?type=f&size=206x290&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240826_273%2F1724636695791h4h0v_JPEG%2Fmovie_image.jpg",
+      vote_average: 6.6,
+    },
+    {
+      title: "사랑의 하츄핑",
+      poster_path:
+        "https://search.pstatic.net/common?type=o&size=304x456&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240702_233%2F1719904004038eLqJl_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2",
+      vote_average: 8.27,
+    },
+    {
+      title: "브레드이발소: 빵스타의 탄생",
+      poster_path:
+        "https://search.pstatic.net/common?type=o&size=304x456&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240820_79%2F1724125791248DVxC2_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2",
+      vote_average: 9.51,
+    },
+    {
+      title: "비긴 어게인",
+      poster_path:
+        "https://search.pstatic.net/common?type=o&size=304x456&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240820_178%2F1724133122047df9TH_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2",
+      vote_average: 9.12,
+    },
+    {
+      title: "트랜스포머 ONE",
+      poster_path:
+        "https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240820_56%2F17241320064047lVYK_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2",
+      vote_average: 8.71,
+    },
+    {
+      title: "룩백",
+      poster_path:
+        "https://search.pstatic.net/common?type=o&size=304x456&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240905_14%2F1725526868331GO1gv_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2",
+      vote_average: 9.08,
+    },
+    {
+      title: "안녕, 할부지",
+      poster_path:
+        "https://search.pstatic.net/common?type=o&size=304x456&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240904_12%2F1725439468297RhV29_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2",
+      vote_average: 8.57,
+    },
+    {
+      title: "소년시절의 너",
+      poster_path:
+        "https://search.pstatic.net/common?type=o&size=304x456&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240730_169%2F1722318789382abguA_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2",
+      vote_average: 9.05,
+    },
+    {
+      title: "파일럿",
+      poster_path:
+        "https://search.pstatic.net/common?type=o&size=304x456&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240731_171%2F1722418168362V2OKt_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2",
+      vote_average: 7.68,
+    },
+    {
+      title: "오지: 사라진 숲을 찾아서",
+      poster_path:
+        "https://search.pstatic.net/common?type=o&size=304x456&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240910_90%2F172593484649486RKF_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2",
+      vote_average: 8.34,
+    },
+  ],
+};
