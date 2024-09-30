981fdc85af2d280d0b146062575e5b5a93872e44
j1m1n-ha <163178666+j1m1n-ha@users.noreply.github.com>
2024-09-30 22:49:26 +0900

    Move: assignment파일 이동

---
+++ 하지민/assignment/src/components/Product.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Product() {
+  return (
+    <div>
+      <img src="." alt="제품 이미지" />
+      <div>제품명</div>
+      <div>가격</div>
+    </div>
+  );
+}
---
+++ 하지민/assignment/src/features/ProductsOnSale.jsx
@@ -0,0 +1,14 @@
+//https://shopping.naver.com/promotion
+import React from "react";
+import Product from "../components/Product";
+
+export default function ProductsOnSale() {
+  return (
+    <div>
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
