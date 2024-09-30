7180f325940ddc98cf36feaf1dfaa66b7d8c6719
suminb99 <suminbaek99@gmail.com>
2024-09-30 17:18:48 +0900

    Feat: 3주차 과제

--- 백수민/과제/wconcept-project/src/App.js
+++ 백수민/과제/wconcept-project/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Product from './components/Product'
+import { dummy } from './productDummy';
 
 function App() {
   return (
     <div>
-      <MainPage />
+      <div className="app-container">
+        {
+          dummy.products.map((item) => {
+            return (
+              <Product 
+                key={item.id}
+                img_path={item.img_path}
+                brand_name={item.brand_name}
+                product_name={item.product_name}
+                price={item.price}
+              />
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ 백수민/과제/wconcept-project/src/productDummy.js
@@ -0,0 +1,84 @@
+export const dummy = {
+    products: [
+        {
+            id: 1,
+            category: 'WOMEN',
+            brand_name: 'OLIVE DES OLIVE',
+            product_name: '라운드넥 부클 트위드 자켓 OW4SJ1500',
+            img_path: 'https://product-image.wconcept.co.kr/productimg/image/img0/60/305408860_GG94191.jpg',
+            price: '193320'
+        },
+        {
+            id: 2,
+            category: 'WOMEN',
+            brand_name: 'ready4next',
+            product_name: '우먼즈 오발 로고 크롭 티셔츠 BLACK',
+            img_path: 'https://product-image.wconcept.co.kr/productimg/image/img0/70/305023270_VL83389.jpg',
+            price: '22500'
+        },
+        {
+            id: 3,
+            category: 'WOMEN',
+            brand_name: 'APOA',
+            product_name: 'Pao Long Handle Bag Crinkle_6 colors',
+            img_path: 'https://product-image.wconcept.co.kr/productimg/image/img0/81/302818381_CN35579.jpg',
+            price: '219300'
+        },
+        {
+            id: 4,
+            category: 'WOMEN',
+            brand_name: 'Ssecondo',
+            product_name: '[단독] Pearl ribbon knot hair claw clip (6 colors)',
+            img_path: 'https://product-image.wconcept.co.kr/productimg/image/img0/80/303307680_HS95037.jpg',
+            price: '19890'
+        },
+        {
+            id: 5,
+            category: 'WOMEN',
+            brand_name: 'edel9',
+            product_name: '[단독]Marigold 메리골드 리본 슬링백 2cm/5cm',
+            img_path: 'https://product-image.wconcept.co.kr/productimg/image/img0/71/305696771_TP76275.jpg',
+            price: '52694'
+        },
+        {
+            id: 6,
+            category: 'MEN',
+            brand_name: 'ADER ERROR',
+            product_name: 'Product. 27 Noir',
+            img_path: 'https://product-image.wconcept.co.kr/productimg/image/img0/31/306102931_KJ12837.jpg',
+            price: '439000'
+        },
+        {
+            id: 7,
+            category: 'MEN',
+            brand_name: 'ready4next',
+            product_name: '오발 로고 레이싱 티셔츠 WHITE',
+            img_path: 'https://product-image.wconcept.co.kr/productimg/image/img0/40/305023240_NS51993.jpg',
+            price: '22500'
+        },
+        {
+            id: 8,
+            category: 'MEN',
+            brand_name: 'NIKE',
+            product_name: '[HJ7245-100] W NIKE V2K RUN',
+            img_path: 'https://product-image.wconcept.co.kr/productimg/image/img0/24/305959724_BM55753.jpg',
+            price: '132050'
+        },
+        {
+            id: 9,
+            category: 'MEN',
+            brand_name: 'Dunst for MEN',
+            product_name: 'UNISEX MULTI STRIPE COLLARED T-SHIRT BURGUNDY STRIPE_M_UDTS4C119D2',
+            img_path: 'https://product-image.wconcept.co.kr/productimg/image/img0/90/306029090_HI16849.jpg',
+            price: '94050'
+        },
+        {
+            id: 10,
+            category: 'MEN',
+            brand_name: 'DIAMOND LAYLA',
+            product_name: 'Tencel Ombre Deep Blue Tartan Check Shirt S161',
+            img_path: 'https://product-image.wconcept.co.kr/productimg/image/img0/61/306011361_LK66619.jpg',
+            price: '124000'
+        },
+    ]
+};
