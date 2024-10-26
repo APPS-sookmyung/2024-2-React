ab487f4e0f691448a87360727249bd0aa1fb0ae3
ninum12 <ninum@sookmyung.ac.kr>
2024-10-26 16:37:11 +0900

    Feat: 6강 실습 코드 및 강의 내용 정리

--- 이다빈/week4-practice/src/App.js
+++ 이다빈/week4-practice/src/App.js
@@ -1,12 +1,34 @@
-import Counter from "./components/Counter";
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  console.log("App 컴포넌트 렌더링 발생");
+
   return (
-    <div className="App">
-      <Counter />
+    <div>
+      부모 컴포넌트
+      <Child />
     </div>
   );
 }
 
-export default App;
+const Child = () => {
+  const [count, setCount] = useState(0);
+
+  console.log("Child 컴포넌트 렌더링 발생");
+
+  return (
+    <div>
+      자식 컴포넌트
+      <span>{count}</span>
+      <button onClick={() => setCount(count + 1)}>state 변경</button>
+      <GrandChild />
+    </div>
+  );
+};
+
+const GrandChild = () => {
+  console.log("GrandChild 컴포넌트 렌더링 발생");
+
+  return <div>손주 컴포넌트</div>;
+};
 
