492b9137c6201f9d3f9a9478a467ca7aee0cb588
ninum12 <ninum@sookmyung.ac.kr>
2024-10-27 10:52:02 +0900

    Feat: 8강 실습 코드 및 강의 내용 정리

--- 이다빈/week4-practice/src/App.js
+++ 이다빈/week4-practice/src/App.js
@@ -1,33 +1,9 @@
-import React from "react";
-import { useState } from "react";
+import List from "./List";
 
 export default function App() {
-  // number
-  const [num, setNum] = useState(0);
-  // string
-  const [name, setName] = useState("");
-  //boolean
-  const [isChecked, setIsChecked] = useState(false);
   return (
     <div>
-      <input
-        type="number"
-        value={num}
-        onChange={(e) => setNum(e.target.value)}
-      />
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <div>
-        <input
-          type="checkbox"
-          value={isChecked}
-          onChange={(e) => setIsChecked(e.target.checked)}
-        />
-        {isChecked ? <span>체크됨</span> : <span>체크안됨</span>}
-      </div>
+      <List />
     </div>
   );
 }
