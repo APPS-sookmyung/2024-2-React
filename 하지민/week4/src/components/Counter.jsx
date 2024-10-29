faccc7b0e344ae6aab4d8e2d8ce16a4e8a3e1b1a
j1m1n-ha <163178666+j1m1n-ha@users.noreply.github.com>
2024-10-29 15:36:27 +0900

    Feat: 6강 실습 코드 및 강의 내용 정리

--- 하지민/week4/src/components/Counter.jsx
+++ 하지민/week4/src/components/Counter.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import { useState } from "react";
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  //   const [count, setCount] = useState(0);
 
+  //   const updateCount = (arg) => {
+  //     setCount(count + arg);
+  //   };
+
+  let count = 0;
   const updateCount = (arg) => {
-    setCount(count + arg);
+    count = count + arg;
+    console.log(count);
   };
 
   return (
