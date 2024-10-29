75b1ce9bdbb558feb61902d84d033470c6d307ed
Bangjh05 <everangel05@naver.com>
2024-10-29 20:25:37 +0900

    Feat: 4주차 강의 내용 정리 및 과제

--- 방지희/practice/src/App.js
+++ 방지희/practice/src/App.js
@@ -1,12 +1,63 @@
-import React from "react";
-import ProductPage from "./components/ProductPage";
+//5강 실습
+// import React from "react";
+// import ProductPage from "./components/Counter";
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <Counter />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//7강 실습
+// import React from 'react';
+// import { useState } from "react";
+
+// export default function App() {
+
+//   //number
+//   const [num, setNum] = useState(0);
+
+//   //string
+//   const [name, setName] = useState('');
+
+//   //boolean
+//   const [isChecked, setIsChecked] = useState(false);
+
+//   return (
+//     <div>
+//       <input
+//       type="number"
+//       value={num}
+//       onChange={(e) => setNum(e.target.value)}
+//       />
+//       <input
+//       type='text'
+//       value={name}
+//       onChange={(e) => setName(e.target.value)}
+//       />
+//       <div>
+//         <input
+//         type='checkbox'
+//         value={isChecked}
+//         onChange={(e) => setIsChecked(e.target.checked)}
+//         />
+//         {isChecked ? <span>체크됨</span> : <span>체크안됨</span>}
+//       </div>
+//     </div>
+//   )
+// }
+
+//8강 실습
+import List from "./List";
+
+export default function App() {
   return (
     <div>
-      <ProductPage />
+      <List />
     </div>
   );
 }
-
-export default App;
